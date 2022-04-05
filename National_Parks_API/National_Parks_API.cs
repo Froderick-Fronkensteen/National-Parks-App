9e881294e8d8738bb0d145e872461dc3557ead54
Michael <42883348+Froderick-Fronkensteen@users.noreply.github.com>
2022-04-04 21:00:29 -0400

    Initial Commit

---
+++ National_Parks_API/National_Parks_API.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+
+
+namespace National_Parks_API
+{
+    public class Parks
+    {
+        
+    }
+}
---
+++ National_Parks_App/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using National_Parks_API;
+
+
+namespace National_Parks_App
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
